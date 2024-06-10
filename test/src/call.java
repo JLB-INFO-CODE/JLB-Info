4ccc52be50ed264b9ca91acccb492e0ae3908cf1
ImCaptainUsopp <35741933+ImCaptainUsopp@users.noreply.github.com>
2024-06-10 12:00:12 +0200

    Structuration_dossier

--- test/src/call.java
+++ test/src/call.java
@@ -1,5 +1,26 @@
-public class call {
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.io.IOException;
+
+
+class call {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes"))
+                .header("X-RapidAPI-Host", "jokes-by-api-ninjas.p.rapidapi.com")
+                .header("X-RapidAPI-Key", "your-rapidapi-key")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(response.body());
     }
 }
